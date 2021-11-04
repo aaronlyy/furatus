ecc86374b20b8a66104ce38f749e509ae2c16153
aaronlyy <aaronlevican@gmail.com>
2021-11-04 14:52:32 +0100

    added description

--- furatus/algorithm.py
+++ furatus/algorithm.py
@@ -2,6 +2,7 @@ import random
 import string
 
 def rand(minlen: int, maxlen: int):
+    """Generates endless random codes (iterable)"""
     chars = list(string.ascii_lowercase)
     chars.extend(string.digits)
     codes = []
